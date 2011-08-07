0a6c2d01b94ea123e8b018a6c39aac43da782359
Zak Amir <blue-eys@hotmail.co.uk>
2011-08-06 20:46:55 -0700

    Fixed naming (1 /2).

---
+++ EyeAccessory/GreenAndRedGlasses.cs
@@ -0,0 +1,10 @@
+namespace IHI.Server.Habbos.Figure.EyeAccessory
+{
+    public class GreenAndRedGlasses : EyeAccessory
+    {
+        public override ushort GetModelID()
+        {
+            return 1406;
+        }
+    }
+}
