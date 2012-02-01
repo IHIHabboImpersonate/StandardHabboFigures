34de5ca9499bcfe2272ee009becba52646df790e
Chris Chenery <Cecer1@cecer1.com>
2012-02-01 11:03:56 +0000

    GPLv3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,41 @@
-﻿using System.Reflection;
+﻿#region GPLv3
+
+// 
+// Copyright (C) 2012  Chris Chenery
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+#endregion
+
+#region Usings
+
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
 [assembly: AssemblyTitle("StandardHabboFigures")]
-[assembly: AssemblyDescription("A plugin for IHI by Zak")]
+[assembly: AssemblyDescription("Default Habbo Figure parts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StandardHabboFigures")]
-[assembly: AssemblyCopyright("Copyright © Zak 2011")]
+[assembly: AssemblyCopyright("Copyright © Cecer1 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
